06ab099d25f469c79f13521cd6f400518b6665e1
Arnav Gudibande <arnav.gudibande@gmail.com>
2016-04-18 21:15:37 -0700

    Final Project for Java Class
    AR Webcam Game

---
+++ Engine.java
@@ -0,0 +1,30 @@
+import javax.swing.JFrame;
+
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
+import com.github.sarxos.webcam.WebcamResolution;
+
+
+public class Engine {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		Webcam webcam = Webcam.getDefault();
+		webcam.setViewSize(WebcamResolution.VGA.getSize());
+		System.out.println("Camera Found!");
+
+		WebcamPanel panel = new WebcamPanel(webcam);
+		panel.setFPSDisplayed(true);
+		panel.setDisplayDebugInfo(true);
+		panel.setImageSizeDisplayed(true);
+		panel.setMirrored(true);
+
+		
+		JFrame window = new JFrame("Test webcam panel");
+		window.add(panel);
+		window.setResizable(true);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.pack();
+		window.setVisible(true);
+	}
+}
